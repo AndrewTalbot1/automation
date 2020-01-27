9b5491e3d3585af1d21860570b34196816eaf467
Fbasskid <andrewtalbot@ymail.com>
2020-01-27 17:42:07 -0600

    Looping through Url using a .txt File

--- url.py
+++ url.py
@@ -8,23 +8,3 @@ with open ("testurls.txt", "r") as txt_file:
             print('There was a problem: %s' % (error))
         else:
             print('This url works:' + ' ' + urls)
-
-
-'''
-urls_list = [
-    "http://www.python.org/no_page_found",
-    "http://www.youtube.com",
-    "http://www.facebook.com",
-    "http://www.baidu.com",
-    "http://www.yahoo.com",
-]
-for urls in urls_list:
-    #print(urls)
-    request = requests.get(urls)
-    try:
-        request.raise_for_status()
-    except Exception as error:
-        print('There was a problem: %s' % (error))
-    else:
-        print('This url works:'+' ' + urls)
-'''
