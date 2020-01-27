135c2e6be7095d31f5ed148c6b0449e88123d1ed
Fbasskid <andrewtalbot@ymail.com>
2020-01-27 17:29:42 -0600

    For loop through a list of URLs and a .txt File

--- url.py
+++ url.py
@@ -1,13 +1,30 @@
 import requests
-urls = requests.get("http://www.python.org/no_page")
-try:
-    urls.raise_for_status()
-except Exception as error:
-    print('There was a problem: %s' % (error))
-else:
-    print('All The urls work')
+with open ("testurls.txt", "r") as txt_file:
+    for urls in txt_file:
+        request = requests.get(urls)
+        try:
+            request.raise_for_status()
+        except Exception as error:
+            print('There was a problem: %s' % (error))
+        else:
+            print('This url works:' + ' ' + urls)
 
 
-
-
-
+'''
+urls_list = [
+    "http://www.python.org/no_page_found",
+    "http://www.youtube.com",
+    "http://www.facebook.com",
+    "http://www.baidu.com",
+    "http://www.yahoo.com",
+]
+for urls in urls_list:
+    #print(urls)
+    request = requests.get(urls)
+    try:
+        request.raise_for_status()
+    except Exception as error:
+        print('There was a problem: %s' % (error))
+    else:
+        print('This url works:'+' ' + urls)
+'''
