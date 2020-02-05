642e4e97e02bf4e084142fe9ed4bb877e454cf62
Fbasskid <andrewtalbot@ymail.com>
2020-02-05 09:22:23 -0600

    Css was random, added code for xpath loop, still need to loop over it

--- webscraper/webscraper/spiders/alibaba_hat_spider.py
+++ webscraper/webscraper/spiders/alibaba_hat_spider.py
@@ -13,16 +13,35 @@ class HatsSpider(scrapy.Spider):
         "AUTOTHROTTLE_START_DELAY": 5.0,
     }
     def parse(self, response):
-        for hat_list in response.css('div.organic-gallery-offer-outter'):
-            yield{
-                "Title": hat_list.css('p.organic-gallery-title__content::text').get(),
-                "Price": hat_list.css('p.gallery-offer-price span::text').get(),
-                "Min Order": hat_list.css('p.gallery-offer-minorder span::text').get(),
-                "Country": hat_list.css("div.organic-gallery-offer-section__seller-tags span::text").get(),
-                "Years In Business": hat_list.css("div.organic-gallery-offer-section__seller-tags span.seller-tag__year::text").get(),
-                "Response Rate": hat_list.css('div.organic-gallery-offer-section__reviews span::text').get(),
-                "Review Score": hat_list.css('div.organic-gallery-offer-section__reviews span.seb-supplier-review__score::text').get(),
-                "Contact Link": response.urljoin(hat_list.css('div.organic-gallery-offer-section__contact a::attr(href)').get()),
+        for hat_list in response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "img-switcher-parent", " " ))]'):
+            yield {
+                "Title": hat_list.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "organic-gallery-title__content", " " ))]/text()').get(),
+                "Price": hat_list.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "gallery-offer-price", " " ))]//span/text()').get(),
+                "Min Order": hat_list.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "gallery-offer-minorder", " " ))]//span/text()').get(),
+                "Country": hat_list.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "bg-visible", " " ))]/text()').get(),
+                "Years In Business": hat_list.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "seller-tag__year", " " ))]/text()').get(),
+                #"Response Rate": hat_list.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "seller-tag__response-rate", " " ))]/text()')[0].get()
+                # Not every listing has a response rate, figure out how to fix categories with NONE
+                "Review Score": hat_list.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "seb-supplier-review__score", " " ))]/text()')[1].get(),
             }
-        #response.css('div.organic-gallery-offer-section__contact a::attr(href)').get()
+        
+        
+        
+        
+# The css response was not as acurate on a this site.    
+# It only looped through a few items in a odd random order     
+        
+    #def parse(self, response):    
+        #for hat_list in response.css('div.organic-gallery-offer-outter'):
+            #yield{
+            #    "Title": hat_list.css('p.organic-gallery-title__content::text').get(),
+            #    "Price": hat_list.css('p.gallery-offer-price span::text').get(),
+            #    "Min Order": hat_list.css('p.gallery-offer-minorder span::text').get(),
+            #    "Country": hat_list.css("div.organic-gallery-offer-section__seller-tags span::text").get(),
+            #    "Years In Business": hat_list.css("div.organic-gallery-offer-section__seller-tags span.seller-tag__year::text").get(),
+            #    "Response Rate": hat_list.css('div.organic-gallery-offer-section__reviews span::text').get(),
+            #    "Review Score": hat_list.css('div.organic-gallery-offer-section__reviews span.seb-supplier-review__score::text').get(),
+            #    "Contact Link": response.urljoin(hat_list.css('div.organic-gallery-offer-section__contact a::attr(href)').get()),
+            #}
+        
         
